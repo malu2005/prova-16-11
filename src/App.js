d1a9442ee99d9acb50c23cb4e15dc41181139dc9
malu2005 <malu.araujosampaio@gmail.com>
2022-11-16 21:29:28 -0300

    prova4

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Menu
+        </h1>
+      </header>
+      <section>
+        <div className='div'>
+          <p>
+            Trocas de Figurinhas da Copa
+          </p>
+          <p>
+            Inverter a Sequência
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
